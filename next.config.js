5be70360a55509f05c69315fe5c126185cff33ea
Plan Ghimire <plan.080bei028@tcioe.edu.np>
2025-02-09 16:39:39 +0545

    fixed built errors

--- next.config.js
+++ next.config.js
@@ -1,24 +1,13 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['media2.dev.to'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-    ],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
   },
 }
